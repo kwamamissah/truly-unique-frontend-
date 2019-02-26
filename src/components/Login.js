8672324def30ec1b21dd12adcf00ba9009eeedd4
github username <github email address>
2019-02-25 22:29:15 -0500

    can sign up from frontend

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,9 +14,22 @@ export default class Login extends Component{
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  handleSubmit = (e) => {
+  handleLoginSubmit = (e) => {
     e.preventDefault()
     fetch(`${API}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(resp => resp.json())
+    .then(console.log)
+  }
+
+  handleSignUpSubmit = (e) => {
+    e.preventDefault()
+    fetch(`${API}/users`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -30,8 +43,10 @@ export default class Login extends Component{
 
   render(){
     return(
-      <Form className='Form' onSubmit={(e) => this.handleSubmit(e)}>
-        {this.props.login ? <h1>Login</h1> : <h1>Signup</h1>}
+      <div>
+      {this.props.login ?
+      <Form onSubmit={(e) => this.handleLoginSubmit(e)}>
+        <h1>Login</h1>
        <Form.Field>
          <input placeholder='Name'
          name='name' onChange={(e) => this.handleChange(e)}
@@ -39,12 +54,28 @@ export default class Login extends Component{
        </Form.Field>
        <Form.Field>
          <input placeholder='Email'
-         name='email' onChange={(e) => this.handleChange(e)}/>
+         name='email' onChange={(e) => this.handleChange(e)} />
        </Form.Field>
        <Button size='small' type='submit'>Submit</Button>
-       {this.props.login ? <Button size='small' onClick={() => this.props.toggleSignup()}>Signup</Button>
-       : <Button size='small' onClick={() => this.props.toggleLogin()}>Login</Button>}
+      <Button className='bc' size='small' onClick={() => this.props.toggleSignup()}>New? Signup</Button>
      </Form>
+        :
+     <Form onSubmit={(e) => this.handleSignUpSubmit(e)}>
+        <h1>Signup</h1>
+      <Form.Field>
+        <input placeholder='Name'
+        name='name' onChange={(e) => this.handleChange(e)}
+         />
+      </Form.Field>
+      <Form.Field>
+        <input placeholder='Email'
+        name='email' onChange={(e) => this.handleChange(e)} />
+      </Form.Field>
+      <Button size='small' type='submit'>Submit</Button>
+      <Button className='bc' size='small' onClick={() => this.props.toggleLogin()}>Login</Button>
+    </Form>
+      }
+    </div>
     )
   }
 }
