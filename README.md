f6c6900c2b19b8c49fdf43472fb51a09c953d1ce
github username <github email address>
2019-02-25 19:34:58 -0500

    made css changes to Login button

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Truly Unique App
 Once logged in the user will be shown a list of options they can choose from. Kind: Poem or Quote && Mood: Love, Encouragement or Funny
 
 May still need to still create login controller on the backend or can just confirm from backend that the email exist.
+
+Affirmation component should ask for the mood and kind of poem and store it in state.
+
+Then use a find over the affirmation array to show a letter with those criteria. Once found save that letter id and the user id to history.
+
+Once the person signs back in save history array to its own state and show last if requested or show new which is affirmation array !== history array. 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Button, Form } from 'semantic-ui-react'
 
+import API from '../backend/data'
 
 export default class Login extends Component{
 
@@ -9,23 +10,40 @@ export default class Login extends Component{
     email: ''
   }
 
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(`${API}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(resp => resp.json())
+    .then(console.log)
+  }
 
 
   render(){
     return(
-      <Form>
+      <Form className='Form' onSubmit={(e) => this.handleSubmit(e)}>
         {this.props.login ? <h1>Login</h1> : <h1>Signup</h1>}
        <Form.Field>
-         <label>Name</label>
-         <input placeholder='Name' />
+         <input placeholder='Name'
+         name='name' onChange={(e) => this.handleChange(e)}
+          />
        </Form.Field>
        <Form.Field>
-         <label>Email</label>
-         <input placeholder='Email' />
+         <input placeholder='Email'
+         name='email' onChange={(e) => this.handleChange(e)}/>
        </Form.Field>
-       <Button type='submit'>Submit</Button>
-       {this.props.login ? <Button onClick={() => this.props.toggleSignup()}>Signup</Button> 
-       : <Button onClick={() => this.props.toggleLogin()}>Login</Button>}
+       <Button size='small' type='submit'>Submit</Button>
+       {this.props.login ? <Button size='small' onClick={() => this.props.toggleSignup()}>Signup</Button>
+       : <Button size='small' onClick={() => this.props.toggleLogin()}>Login</Button>}
      </Form>
     )
   }
